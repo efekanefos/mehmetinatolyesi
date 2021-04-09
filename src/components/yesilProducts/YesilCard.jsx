502aa624f79ef0a8b44decb0c3e27b0bff783648
Efekan EFE <54044105+efekanefos@users.noreply.github.com>
2021-04-09 12:55:55 +0300

    Trendyol%Shopier_Buttons_Added

--- src/components/yesilProducts/YesilCard.jsx
+++ src/components/yesilProducts/YesilCard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
+/*
 import { Link } from "react-router-dom";
+*/
 import "../../style/productcard.css";
+import shopier_logo from "../../images/brands/shopier_logo.png";
+import trendyol_logo from "../../images/brands/trendyol_logo.png";
 
 function YesilCard(props) {
   return (
@@ -12,20 +16,34 @@ function YesilCard(props) {
           variant="top"
           src={props.imgsrc}
         />
-        <Card.Text className="text-center">
-          Ürün İsmi <br />
-          Ürünün Fiyatı
-        </Card.Text>
-        <Link
-          to={`/yesilcam/${props.id}`}
-          className="btn btn-danger d-block mx-auto"
-          id="readmore"
-        >
-          Satın Al
-        </Link>
+        <a href="https://www.trendyol.com">
+          <Button className="btn-shopier mb-2">
+            <img width="70" height="40" src={shopier_logo} alt="shopier" />
+            <span>'da İncele</span>
+          </Button>
+        </a>
+        <a href="https://www.shopier.com/ShowProductNew/storefront.php?shop=Mehmetin_atolye&sid=R2Y1R2ZCU2FhdDdremNTQTFfLTFfIF8g">
+          <Button className="btn-trendyol btn-light">
+            <img width="80" src={trendyol_logo} alt="trendyol" />
+            <span>'da İncele</span>
+          </Button>
+        </a>
       </Card>
     </div>
   );
 }
 
 export default YesilCard;
+/* 
+<Card.Text className="text-center">
+          Ürün İsmi <br />
+          Ürünün Fiyatı
+        </Card.Text>
+<Link
+          to={`/yesilcam/${props.id}`}
+          className="btn btn-danger d-block mx-auto"
+          id="readmore"
+        >
+          Satın Al
+        </Link>
+*/
